8c2e7e965145155ecc0cbe52e1d961b8f0590ee7
tuan anh <tuananhbui0703@gmail.com>
2026-04-07 18:44:32 +0300

    change environment variable name

--- frontend/components/Form.jsx
+++ frontend/components/Form.jsx
@@ -1,7 +1,7 @@
 import { useState,useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.API_BASE_URL //|| 'http://localhost:3001';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL //|| 'http://localhost:3001';
 const baseUrl = `${API_BASE_URL}/colleges/recommend`;
 
 const analysisSections = [
