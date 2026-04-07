9c0bfff8b0d81c847552668e119c01671623f744
tuan anh <tuananhbui0703@gmail.com>
2026-04-07 18:50:18 +0300

    added cors

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,9 @@ import { initializeEmbeddings } from './scripts/initAnchor.js'
 
 const app = express()
 app.use(express.json()) 
-app.use(cors())
+app.use(cors({
+    origin: 'https://project-us-college.vercel.app/'
+}))
 
 await initializeEmbeddings();
 app.use('/colleges', collegeRouter)
